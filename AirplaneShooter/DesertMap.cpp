4899259b202d6c1b9ba9397e151d67c8dc1ce110
Daniel Vicarel <shundra8820@gmail.com>
2015-06-06 16:17:03 -0400

    Added using statements to all .cpp files.  Added some solution folders to organize files

--- AirplaneShooter/DesertMap.cpp
+++ AirplaneShooter/DesertMap.cpp
@@ -2,14 +2,17 @@
 #include "Aircraft.h"
 #include <Brush.h>
 
+using namespace sf;
+using namespace Game2D;
+
 namespace Shooter {
 
 	// CONSTRUCTORS / DESTRUCTOR
-	DesertMap::DesertMap(sf::RenderWindow* window) :
-		Game2D::ScrollingMap(
-			sf::Vector2f(0.f, -50.f),
+	DesertMap::DesertMap(RenderWindow* window) :
+		ScrollingMap(
+			Vector2f(0.f, -50.f),
 			window,
-			sf::FloatRect(0.f, 0.f, window->getDefaultView().getSize().x, 2000.f),
+			FloatRect(0.f, 0.f, window->getDefaultView().getSize().x, 2000.f),
 			3),
 		_playerSpeed(40.f)
 	{
@@ -17,26 +20,26 @@ namespace Shooter {
 		_view = _window->getDefaultView();
 		float playerSpawnX = _worldBounds.left + _view.getSize().x / 2.f;
 		float playerSpawnY = _worldBounds.top + _worldBounds.height - _view.getSize().y / 2.f;
-		_playerSpawn = sf::Vector2f(playerSpawnX, playerSpawnY);
+		_playerSpawn = Vector2f(playerSpawnX, playerSpawnY);
 		_view.setCenter(_playerSpawn);
 	}
 	DesertMap::~DesertMap() {}
 
 	// INTERFACE FUNCTIONS
-	void DesertMap::updateCurrent(sf::Time dt) {
+	void DesertMap::updateCurrent(Time dt) {
 
 		// Adjust the player's velocity according to keyboard input
-		_player->Velocity = sf::Vector2f(0.f, 0.f);
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-			_player->Velocity = -sf::Vector2f(_playerSpeed, 0.f);
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-			_player->Velocity = sf::Vector2f(_playerSpeed, 0.f);
+		_player->Velocity = Vector2f(0.f, 0.f);
+		if (Keyboard::isKeyPressed(Keyboard::A) || Keyboard::isKeyPressed(Keyboard::Left))
+			_player->Velocity = -Vector2f(_playerSpeed, 0.f);
+		if (Keyboard::isKeyPressed(Keyboard::D) || Keyboard::isKeyPressed(Keyboard::Right))
+			_player->Velocity = Vector2f(_playerSpeed, 0.f);
 
 		// Adjust the player's velocity according to their position in the Map
 		float playerPos = _player->getPosition().x;
 		if (playerPos <= _worldBounds.left + BOUNDARY_OFFSET || _worldBounds.left + _worldBounds.width - BOUNDARY_OFFSET <= playerPos) {
 			float reverse = -_player->Velocity.x;
-			_player->Velocity = sf::Vector2f(reverse, 0.f);
+			_player->Velocity = Vector2f(reverse, 0.f);
 		}
 
 		// Scroll the map
@@ -52,11 +55,11 @@ namespace Shooter {
 	}
 	void DesertMap::buildScene() {
 		// Add a node for the background, and tile its texture
-		sf::Texture& desertTexture = _textures[ResourceIDs::Texture::DESERT];
-		sf::IntRect desertBounds(_worldBounds);
+		Texture& desertTexture = _textures[ResourceIDs::Texture::DESERT];
+		IntRect desertBounds(_worldBounds);
 		desertTexture.setRepeated(true);
-		Game2D::Brush::Ptr background(
-			new Game2D::Brush(desertTexture, desertBounds));
+		Brush::Ptr background(
+			new Brush(desertTexture, desertBounds));
 		background->setPosition(_worldBounds.left, _worldBounds.top);
 		_sceneLayers[BACKGROUND]->attachChild(std::move(background));
 
