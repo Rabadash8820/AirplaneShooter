c0b22e723915fee08d943ad5bbff27bc397878af
Daniel Vicarel <shundra8820@gmail.com>
2015-07-18 00:17:31 -0400

    Came up with better implementation for game Categories.  Added InputBinding class

--- Solution/Game2D/Category.h
+++ Solution/Game2D/Category.h
@@ -1,12 +1,22 @@
+#pragma once
+
+#include "GAME2D_API.h"
 namespace Game2D {
 
-	namespace Category {
+	class GAME2D_API Category {
+	private:
+		unsigned int _id;
+		static unsigned int _root;
 
-		const char* Nothing = "Nothing";
-		const char* Node = "Node";
-		const char* Brush = "Brush";
-		const char* Entity = "Entity";
+	public:
+		Category() {
+			_id = _root << 1;
+		}
+		unsigned int id() {
+			return _id;
+		}
+	};
 
-	}
+	unsigned int Category::_root = 1;
 
 }
