528b87cdf95dcd35613aff71e23c11e35027c01f
Daniel Vicarel <shundra8820@gmail.com>
2015-07-21 01:44:04 -0400

    Just a commit to clean up that last merge

--- Solution/AirplaneShooter/DesertMap.cpp
+++ Solution/AirplaneShooter/DesertMap.cpp
@@ -4,6 +4,7 @@
 #include <Render/Brush.h>
 #include <stdio.h>  /* defines FILENAME_MAX */
 #include <cassert>
+#include <iostream>
 
 // Define a general macro to get the current working directory
 #ifdef _WIN32
@@ -14,27 +15,27 @@
 #define GetCurrentDir getcwd
 #endif
 
-using namespace sf;
+using namespace Shooter;
 using namespace Game2D;
 using namespace sf;
 using namespace std;
 
 // INTERFACE
-	DesertMap::DesertMap(RenderWindow* window) :
-		ScrollingMap(
-			Vector2f(0.f, -50.f),
-			window,
-			FloatRect(0.f, 0.f, window->getDefaultView().getSize().x, 2000.f),
-			3),
-		_playerSpeed(40.f)
-	{
-		// Align the player spawn point with the center of the View
-		_view = _window->getDefaultView();
-		float playerSpawnX = _worldBounds.left + _view.getSize().x / 2.f;
-		float playerSpawnY = _worldBounds.top + _worldBounds.height - _view.getSize().y / 2.f;
-		_playerSpawn = Vector2f(playerSpawnX, playerSpawnY);
-		_view.setCenter(_playerSpawn);
-	}
+DesertMap::DesertMap(RenderWindow* window) :
+	ScrollingMap(
+		Vector2f(0.f, -50.f),
+		window,
+		FloatRect(0.f, 0.f, window->getDefaultView().getSize().x, 2000.f),
+		3),
+	_playerSpeed(40.f)
+{
+	// Align the player spawn point with the center of the View
+	_view = _window->getDefaultView();
+	float playerSpawnX = _worldBounds.left + _view.getSize().x / 2.f;
+	float playerSpawnY = _worldBounds.top + _worldBounds.height - _view.getSize().y / 2.f;
+	_playerSpawn = Vector2f(playerSpawnX, playerSpawnY);
+	_view.setCenter(_playerSpawn);
+}
 void DesertMap::handleEvent(const sf::Event& e) {
 	Shooter::Categories* c = reinterpret_cast<Shooter::Categories*>(_categories.get());
 	switch (e.type) {
@@ -61,51 +62,13 @@ void DesertMap::handleRealtimeInput() {
 		_commands.push(moveLeft);
 		}
 	}
-DesertMap::~DesertMap() {}
-
-	// HELPER FUNCTIONS
-	void DesertMap::loadResources() {
-		// Get resource directories
-		string textureDir = projectDirectory() + "\\Resources\\Textures\\";
-
-		// Load texture resources
-		_textures.load(ResourceIDs::Texture::DESERT, textureDir + "Desert.png");
-		_textures.load(ResourceIDs::Texture::EAGLE,  textureDir + "Eagle.png");
-		_textures.load(ResourceIDs::Texture::RAPTOR, textureDir + "Raptor.png");
-	}
-	void DesertMap::buildScene() {
-		// Add a node for the background, and tile its texture
-		Texture& desertTexture = _textures[ResourceIDs::Texture::DESERT];
-		IntRect desertBounds(_worldBounds);
-		desertTexture.setRepeated(true);
-		Brush::Ptr background(
-		new Brush(desertTexture, desertBounds, unique_ptr<Categories>(new Categories())));
-		background->setPosition(_worldBounds.left, _worldBounds.top);
-		_sceneLayers[BACKGROUND]->attachChild(std::move(background));
-
-		// Add a node for the leader Aircraft and assign it to the Player
-		Aircraft::Ptr leader(new Aircraft(Aircraft::EAGLE, _textures));
-		leader->setPosition(_playerSpawn);
-		_player = leader.get();
-		_sceneLayers[AIR]->attachChild(std::move(leader));
-
-		// Add nodes for the player's escort Aircraft
-		Aircraft::Ptr leftEscort(new Aircraft(Aircraft::RAPTOR, _textures));
-		Aircraft::Ptr rightEscort(new Aircraft(Aircraft::RAPTOR, _textures));
-		leftEscort->setPosition(-80.f, 50.f);
-		rightEscort->setPosition(80.f, 50.f);
-		_player->attachChild(std::move(leftEscort));
-		_player->attachChild(std::move(rightEscort));
-
-		// Make things scroll
-		ScrollingMap::buildScene();
-	}
-	string DesertMap::projectDirectory() {
-		char currDir[FILENAME_MAX];
-		GetCurrentDir(currDir, sizeof(currDir));
-		// Make some assertion here...
-		return currDir;
-	}
+void DesertMap::updateCurrent(Time dt) {
+	// Adjust the player's velocity according to keyboard input
+	_player->Velocity = -Vector2f(0.f, 0.f);
+	if (Keyboard::isKeyPressed(Keyboard::A) || Keyboard::isKeyPressed(Keyboard::Left))
+		_player->Velocity = -Vector2f(_playerSpeed, 0.f);
+	if (Keyboard::isKeyPressed(Keyboard::D) || Keyboard::isKeyPressed(Keyboard::Right))
+		_player->Velocity = Vector2f(_playerSpeed, 0.f);
 
 	// Adjust the player's velocity according to their position in the Map
 	float playerPos = _player->getPosition().x;
@@ -113,4 +76,52 @@ DesertMap::~DesertMap() {}
 		float reverse = -_player->Velocity.x;
 		_player->Velocity = Vector2f(reverse, 0.f);
 	}
+
+	// Scroll the map
+	ScrollingMap::updateCurrent(dt);
+}
+DesertMap::~DesertMap() {}
+
+// HELPER FUNCTIONS
+void DesertMap::loadResources() {
+	// Get resource directories
+	string textureDir = projectDirectory() + "\\Resources\\Textures\\";
+
+	// Load texture resources
+	_textures.load(ResourceIDs::Texture::DESERT, textureDir + "Desert.png");
+	_textures.load(ResourceIDs::Texture::EAGLE,  textureDir + "Eagle.png");
+	_textures.load(ResourceIDs::Texture::RAPTOR, textureDir + "Raptor.png");
+}
+void DesertMap::buildScene() {
+	// Add a node for the background, and tile its texture
+	Texture& desertTexture = _textures[ResourceIDs::Texture::DESERT];
+	IntRect desertBounds(_worldBounds);
+	desertTexture.setRepeated(true);
+	Brush::Ptr background(
+	new Brush(desertTexture, desertBounds, unique_ptr<Categories>(new Categories())));
+	background->setPosition(_worldBounds.left, _worldBounds.top);
+	_sceneLayers[BACKGROUND]->attachChild(std::move(background));
+
+	// Add a node for the leader Aircraft and assign it to the Player
+	Aircraft::Ptr leader(new Aircraft(Aircraft::EAGLE, _textures));
+	leader->setPosition(_playerSpawn);
+	_player = leader.get();
+	_sceneLayers[AIR]->attachChild(std::move(leader));
+
+	// Add nodes for the player's escort Aircraft
+	Aircraft::Ptr leftEscort(new Aircraft(Aircraft::RAPTOR, _textures));
+	Aircraft::Ptr rightEscort(new Aircraft(Aircraft::RAPTOR, _textures));
+	leftEscort->setPosition(-80.f, 50.f);
+	rightEscort->setPosition(80.f, 50.f);
+	_player->attachChild(std::move(leftEscort));
+	_player->attachChild(std::move(rightEscort));
+
+	// Make things scroll
+	ScrollingMap::buildScene();
+}
+string DesertMap::projectDirectory() {
+	char currDir[FILENAME_MAX];
+	GetCurrentDir(currDir, sizeof(currDir));
+	// Make some assertion here...
+	return currDir;
 }
