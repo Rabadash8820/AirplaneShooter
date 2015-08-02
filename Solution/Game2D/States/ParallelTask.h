27bac35e0ad7e47bcb6c7e8ac3583ff8210e124d
Daniel Vicarel <shundra8820@gmail.com>
2015-08-02 03:47:48 -0400

    Began implementation of ParallelTasks...to be continued WAY later

--- Solution/Game2D/States/ParallelTask.h
+++ Solution/Game2D/States/ParallelTask.h
@@ -3,16 +3,33 @@
 #include "..\GAME2D_API.h"
 
 #include <SFML\System\Thread.hpp>
+#include <SFML\System\Clock.hpp>
+#include <SFML\System\Mutex.hpp>
+#include <SFML\System\Time.hpp>
 
 namespace Game2D {
 
 	class GAME2D_API ParallelTask {
+		// ENCAPSULATED FIELDS
+	private:
+		sf::Thread _thread;
+		bool _finished;
+		sf::Clock _elapsedTime;
+		sf::Mutex _mutex;
+
 		// INTERFACE
 	public:
 		ParallelTask();
-		float getProgress();
+		virtual float getProgress() = 0;
 		void execute();
 		bool isFinished();
+		sf::Time getElapsedTime();
+
+		// HELPER FUNCTIONS
+	protected:
+		virtual void doWork() = 0;
+	private:
+		void runTask();
 	};
 
 }
